bea86aa406da1db52cc52cf3dce39f33b563fcbf
refatahmed441 <refatahmed441@gmail.com>
2023-10-09 01:14:59 +0600

    1st

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//  side nav start  
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  //  side nav end  
